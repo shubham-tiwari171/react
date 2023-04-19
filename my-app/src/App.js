8de89e198c727d73713eb60a39e37a94708f1123
Shubham <tshubhamucem@gmail.com>
2023-04-19 23:55:10 +0530

    solution-1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src='./logo192.png' alt="logo" />
+      <div className="content">
+        <div className="adjustContent">
+          <h1>Benifits if using react....</h1>
+          <h3>1.Component based Architechture</h3>
+          <h3>2.Virtual Dom for efficient updates</h3>
+          <h3>3.Rich ecosystem and community</h3>
+          <h3>4.Cross platform development</h3>
+          <h3>5.Strongly community support</h3>
+        </div>
+      </div>
+      <button>Get Started....</button>
     </div>
   );
 }
