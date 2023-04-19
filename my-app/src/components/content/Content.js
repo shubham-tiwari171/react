cc912c071827915c00cf6514b30299e143027dfb
Shubham <tshubhamucem@gmail.com>
2023-04-20 00:17:03 +0530

    solution 2

---
+++ my-app/src/components/content/Content.js
@@ -0,0 +1,14 @@
+function Content() {
+    return (
+        <div className="content">
+            <div className="adjustContent">
+                <h1>Benifits if using react....</h1>
+                <h3>1.Component based Architechture</h3>
+                <h3>2.Virtual Dom for efficient updates</h3>
+                <h3>3.Rich ecosystem and community</h3>
+                <h3>4.Cross platform development</h3>
+                <h3>5.Strongly community support</h3>
+            </div>
+        </div>)
+}
+export default Content;
---
+++ my-app/src/components/logo/Logo.js
@@ -0,0 +1,4 @@
+function Logo() {
+    return (<img src='./logo192.png' alt="logo" />)
+}
+export default Logo;
