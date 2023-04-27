3c674a3371010fb3f5c47d86397e6399e30c9e78
Shubham <tshubhamucem@gmail.com>
2023-04-27 20:03:15 +0530

    name upader

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [randomNumber, setrandomNumber] = useState(Math.floor(Math.random() * 10) + 1);
+  const [guessNumber, setguessNumber] = useState("");
+  const [count, setcount] = useState(0);
+  let localCount = 0;
+  function handleOnChange(event) {
+    event.preventDefault();
+    setguessNumber(event.target.value);
+  }
+
+  function handleClick() {
+    if (randomNumber === Number(guessNumber)) {
+      alert("Congratulations you won");
+      setcount(count + 1);
+    }
+    else {
+      localCount = localCount + 1;
+      console.log(localCount);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="alignments">
+        <div className="content">
+          <div className="gap"><h2>Name updater</h2></div>
+          <div className="d-flex justify-content-between">
+            <div>
+              <input type="text" className="form-control" placeholder="Enter Number to guess" onChange={handleOnChange} />
+            </div>
+            <div><button className="btn btn-primary" onClick={handleClick}>Submit</button ></div>
+          </div>
+          <div className="gap"><h4>Hii my name is: {count}</h4></div>
+          <div className="gap"><h4>random: {randomNumber}</h4></div>
+        </div>
+      </div >
+    </>
   );
 }
 
