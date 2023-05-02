48476f17d3bf55bb24b5bfb69b8a8129f848ff26
Shubham <tshubhamucem@gmail.com>
2023-05-02 21:54:42 +0530

    image change on referesh

---
+++ my-app/src/Components/section/Dogfectchapi/Dogfecth.js
@@ -0,0 +1,24 @@
+import './Dogfetch.css'
+import { useEffect, useRef, useState } from 'react';
+export function DogFetcher() {
+    const imageRef = useRef('');
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((data) => data.json())
+            .then((response) => imageRef.current.src = response.message)
+            .catch((error) => console.log(error))
+    }, [])
+
+    return (
+
+        <>
+            <div className="card alignments">
+                <div className="content d-flex justify-content-center align-items-center">
+                    <img className="image-center" ref={imageRef} alt="dog image" />
+                </div>
+            </div>
+        </>
+
+    );
+}
