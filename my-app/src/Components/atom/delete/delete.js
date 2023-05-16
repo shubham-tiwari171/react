9a3f6f737b0769096bd1f15ea30ee8d13d453c22
Shubham <tshubhamucem@gmail.com>
2023-05-16 23:30:17 +0530

    crud app

---
+++ my-app/src/Components/atom/delete/delete.js
@@ -0,0 +1,42 @@
+import { MdDelete } from 'react-icons/md'
+import styles from './delete.module.css'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export function Delete() {
+    const [todo, setTodo] = useState([])
+    const [data, setData] = useState('')
+    const handleChange = (e) => {
+        setData(e.target.value)
+    }
+    const handleClick = () => {
+        let dataObject = { id: uuidv4(), name: data };
+        setTodo([...todo, dataObject]);
+        setData('');
+    }
+    const handleDeleteClick = (id) => {
+        console.log(id)
+        const updatedTodo = todo.filter((item) => item.id !== id);
+        setTodo(updatedTodo);
+    }
+    return (
+        <>
+            <div className='d-flex flex-column'>
+                {
+                    todo.map((item) => (
+                        <div id={item.id} key={item.id} className={`d-flex justify-content-evenly align-items-center ${styles.container}`}>
+                            <div className={`${styles.left}`}><span>{item.name}</span></div>
+                            <div className={`${styles.right}`} onClick={() => handleDeleteClick(item.id)}>{<MdDelete size={25} />}</div>
+                        </div>
+
+                    ))
+                }
+
+                <div className={`d-flex justify-content-center align-items-center ${styles.input}`}>
+                    <input type="text" value={data} className="form-control m-auto" name="add" id="add" onChange={handleChange} />
+                    <button className="btn btn-outline-dark" style={{ height: "100%" }} onClick={handleClick}>Add</button>
+                </div >
+            </div >
+        </>
+    )
+}
