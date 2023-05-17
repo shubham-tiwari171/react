56501478127dc1ca38fe3d01a9417613500be2cd
Shubham <tshubhamucem@gmail.com>
2023-05-17 16:40:45 +0530

    added css

--- my-app/src/Components/atom/delete/delete.js
+++ my-app/src/Components/atom/delete/delete.js
@@ -1,23 +1,33 @@
 import { MdDelete } from 'react-icons/md'
 import styles from './delete.module.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 export function Delete() {
     const [todo, setTodo] = useState([])
     const [data, setData] = useState('')
+
+
     const handleChange = (e) => {
         setData(e.target.value)
     }
+
     const handleClick = () => {
         let dataObject = { id: uuidv4(), name: data };
         setTodo([...todo, dataObject]);
         setData('');
     }
+
     const handleDeleteClick = (id) => {
         console.log(id)
         const updatedTodo = todo.filter((item) => item.id !== id);
         setTodo(updatedTodo);
+    }
+    const getData = () => {
+        let locaStorageData = localStorage.getItem("todo");
+        if (locaStorageData) {
+            return JSON.parse(locaStorageData)
+        }
     }
     return (
         <>
@@ -25,7 +35,7 @@ export function Delete() {
                 {
                     todo.map((item) => (
                         <div id={item.id} key={item.id} className={`d-flex justify-content-evenly align-items-center ${styles.container}`}>
-                            <div className={`${styles.left}`}><span>{item.name}</span></div>
+                            <div className={`${styles.left}`}><div>{item.name}</div></div>
                             <div className={`${styles.right}`} onClick={() => handleDeleteClick(item.id)}>{<MdDelete size={25} />}</div>
                         </div>
 
