fb63ae29bbd053e682cd8c5c4d26c858adf13494
Shubham <tshubhamucem@gmail.com>
2023-05-28 18:17:49 +0530

    gatting value from register form

--- my-app/src/Components/molecule/Register/Register.jsx
+++ my-app/src/Components/molecule/Register/Register.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
+
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phone: "",
+    city: "paris",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center align-items-center">
         <div className="form-container" style={{ marginTop: "100px" }}>
           <h3 className="title">Register</h3>
-          <form className="form-horizontal">
+          <form className="form-horizontal" onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username">User Name</label>
               <input
@@ -14,6 +33,8 @@ const Register = () => {
                 className="form-control"
                 id="username"
                 placeholder="User Name"
+                value={formData.username}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group">
@@ -23,6 +44,8 @@ const Register = () => {
                 className="form-control"
                 id="email"
                 placeholder="Email Address"
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group">
@@ -32,6 +55,8 @@ const Register = () => {
                 className="form-control"
                 id="password"
                 placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group">
@@ -39,8 +64,10 @@ const Register = () => {
               <input
                 type="password"
                 className="form-control"
-                id="confirm-password"
+                id="confirmPassword"
                 placeholder="Confirm Password"
+                value={formData.confirmPassword}
+                onChange={handleChange}
               />
             </div>
             <h4 className="sub-title">Personal Information</h4>
@@ -51,11 +78,18 @@ const Register = () => {
                 className="form-control"
                 id="phone"
                 placeholder="Phone Number"
+                value={formData.phone}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group">
               <label htmlFor="city">City</label>
-              <select className="form-control" id="city">
+              <select
+                className="form-control"
+                id="city"
+                value={formData.city}
+                onChange={handleChange}
+              >
                 <option value="paris">Paris</option>
                 <option value="new york">New York</option>
               </select>
@@ -64,12 +98,14 @@ const Register = () => {
             <span className="signin-link">
               Already have an account? Click here to <a href="">Login</a>
             </span>
-            <button className="btn signup">Create Account</button>
+            <button type="submit" className="btn signup">
+              Create Account
+            </button>
           </form>
         </div>
       </div>
-      //{" "}
     </>
   );
 };
+
 export default Register;
