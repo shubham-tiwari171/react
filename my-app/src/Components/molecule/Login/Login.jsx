66eea9ae0352a79d3ceac3553e783954070a3a22
Shubham <tshubhamucem@gmail.com>
2023-05-28 18:45:32 +0530

    creatinf form object

--- my-app/src/Components/molecule/Login/Login.jsx
+++ my-app/src/Components/molecule/Login/Login.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./Login.css";
+import { useState } from "react";
 const Login = () => {
+  const [loginForm, setLoginForm] = useState({
+    id: "",
+    email: "",
+    password: "",
+  });
+
+  const handleLoginFormSubmit = (e) => {
+    setLoginForm({ ...loginForm, [e.target.id]: e.target.value });
+  };
+
+  const handleCahange = () => {};
+
   return (
     <>
       <div className="d-flex justify-content-center align-items-center">
         <div className="form-container" style={{ marginTop: "100px" }}>
           <h3 className="title">Register</h3>
-          <form className="form-horizontal">
+          <form className="form-horizontal" onSubmit={handleLoginFormSubmit}>
             <div className="form-group">
               <label htmlFor="email">Email ID</label>
               <input
@@ -14,6 +27,7 @@ const Login = () => {
                 className="form-control"
                 id="email"
                 placeholder="Email Address"
+                onChange={handleCahange}
               />
             </div>
             <div className="form-group">
@@ -23,6 +37,7 @@ const Login = () => {
                 className="form-control"
                 id="password"
                 placeholder="Password"
+                onChange={handleCahange}
               />
             </div>
             <span className="signin-link">
